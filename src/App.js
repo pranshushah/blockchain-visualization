afdf0dc4d3260cb7f89f65d6b9a1ca6a67331163
pranshu shah <pranshu.shah23@gmail.com>
2020-05-18 17:13:29 +0530

    added scrips for github pages and added hash router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { lazy, Suspense } from 'react';
 import NavBar from './component/NavBar/NavBar';
 import {
-  BrowserRouter as Router,
+  HashRouter as Router,
   Switch,
   Route,
   Redirect,
