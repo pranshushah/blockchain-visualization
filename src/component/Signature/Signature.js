6841bf0f2faa48228addd472598c3d9c34802dd7
pranshu shah <pranshu.shah23@gmail.com>
2020-06-03 15:29:06 +0530

    signuature started

--- src/component/Signature/Signature.js
+++ src/component/Signature/Signature.js
@@ -1,1 +1,8 @@
+import React from 'react';
+import { Header } from 'semantic-ui-react';
 
+// TODO : implement Signature.
+
+export default function Signature() {
+  return <Header>Comming Soon</Header>;
+}
